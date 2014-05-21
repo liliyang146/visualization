122b20c659bcbdd007c53de9e3704fb254328b17
unknown <tmuller@client-w7.wscert.com>
2014-05-21 14:17:51 -0700

    New Java file

---
+++ src/main/java/app/controllers/Application.java
@@ -0,0 +1,38 @@
+package controllers;
+
+import java.util.List;
+
+import models.Book;
+import play.data.Form;
+import play.db.ebean.Model;
+import play.libs.Json;
+import play.mvc.BodyParser;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.index;
+
+
+public class Application extends Controller {
+
+
+	public static Result index() {
+		return ok(index.render("Ai, aziti, murmuliti!"));
+	}
+
+	public static Result addBook() {
+		Book book = Form.form(Book.class).bindFromRequest().get();
+		book.save();
+		return redirect(routes.Application.index());
+	}
+
+
+	
+	@BodyParser.Of(play.mvc.BodyParser.Json.class)
+	public static Result getBooks() {
+		List<Book> bookList = new Model.Finder<String, Book>(String.class,
+				Book.class).all();
+		return ok(Json.toJson(bookList));
+	}
+		
+
+}
